b6d366182ec50706e31676d24817231949ead266
Ekat1 <59381663+Ekat1@users.noreply.github.com>
2021-05-28 07:33:08 -0700

    Serves as a module with customizable levels
    yay...

--- platformer.py
+++ platformer.py
@@ -1,94 +1,150 @@
-#make sure to install pygame before running this. To do so on windows 10, open command prompt and type "py -m pip install pygame"
-import pygame
-#initial variables
-x = 500
-y = 500
-xvel = 0
-yvel = 0
-speed = 8
-jump_height = 15
-platforms = []
-ground = False
-window = pygame.display.set_mode((1500, 1000))
-#Set platform generation
-platforms.append((0,750,1500,250))
-platforms.append((1300,700,200,50))
-platforms.append((1000,640,200,50))
-
-                     
-run = True
 #Main game loop
-while run:
-    window.fill((255,255,255)) #window color
-    pygame.draw.rect(window,(175, 0, 255),(x,y,50,50)) #draw player
-    
-    #keyboard input system for player movement. Arrow keys to move
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-    keys = pygame.key.get_pressed()
-    #Changing y and x velocity instead of just y and x to add momentum and gravity
-    if keys[pygame.K_LEFT]:
-        if xvel >= -10:
-            xvel -= speed
-        if xvel < -10:
-            xvel = -10
-    if keys[pygame.K_RIGHT]:
-        if xvel <= 10:
-            xvel += speed
-        if xvel > 10:
-            xvel = 10
-    if keys[pygame.K_UP]:
-        if ground == True:
-            yvel = jump_height * -1
-    
-    #Platform hitboxes(currently not functional under platforms)
+platforms = []
 
-    for platform in platforms:
-        pygame.draw.rect(window,(0, 0, 0),(platform[0],platform[1],platform[2],platform[3]))
-        #top and bottom hitbox
-        if x + xvel > platform[0] - 50:
-            if x + xvel < platform[0] + platform[2]:
-                if y <= platform[1] - 50:
-                    if y + yvel >= platform[1] - 50:
-                        ground = True
-                        y = platform[1] - 50
-                        yvel = 0
-                    else:
-                        ground = False
+class system:
+
+    def build_platform(x,y,width,height,level):
+        platforms.append((x,y,width,height,level))
+        
+    
+    def main(x,y,speed,jump_height,windowx,windowy,level_num):
+        import pygame
+        startx = x
+        starty = y
+        level = 1
+        xvel = 0
+        yvel = 0
+        ground = False
+        window = pygame.display.set_mode((windowx, windowy))
+        player = pygame.image.load('player.png')
+        top = pygame.image.load('top.png')
+        groundtex = pygame.image.load('ground.png')
+        run = True
+        while run:
+            window.fill((255,255,255)) #window color#draw player
+            window.blit(player, (x, y))
+    
+            #keyboard input system for player movement. Arrow keys to move
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            keys = pygame.key.get_pressed()
+            #Changing y and x velocity instead of just y and x to add momentum and gravity
+            if keys[pygame.K_LEFT]:
+                if xvel >= speed * -1:
+                    xvel -= 2
+                if xvel < -10:
+                    xvel = -10
+            if keys[pygame.K_RIGHT]:
+                if xvel <= speed:
+                    xvel += 2
+                if xvel > 10:
+                    xvel = 10
+            if keys[pygame.K_UP]:
+                if ground == True:
+                    yvel = jump_height * -1
+    
+            #Platform hitboxes
+
+            for platform in platforms:
+                if platform[4] == level:
+                    repeat = int(platform[2] / 50)
+                    repeaty = int(platform[3] / 50)
+                    start = platform[0]
+                    startdwn = platform[1]
+                    for i in range(repeaty):
+                        for i in range(repeat):
+                            window.blit(groundtex, (start, startdwn))
+                            start += 50
+                        cropground = pygame.Surface(((platform[2] - (repeat * 50)), 50))
+                        cropground.blit(groundtex, (0, 0), (0, 0, (platform[2] - (repeat * 50)), 50))
+                        window.blit(cropground, (start, startdwn))
+                        startdwn += 50
+                        start = platform[0]
+                    cropgrounddwn = pygame.Surface((platform[2], (platform[3] - (repeaty * 50))))
+                    start = 0
+                    for i in range(repeat):
+                        cropgrounddwn.blit(groundtex, (start, 0))
+                        start += 50
+                    window.blit(cropgrounddwn, (platform[0], startdwn))
+
+                        
+                    start = platform[0]
+                    for i in range(repeat):
+                        window.blit(top, (start, platform[1]))
+                        start += 50
+                    croptop = pygame.Surface(((platform[2] - (repeat * 50)), 50), pygame.SRCALPHA)
+                    croptop.blit(top, (0, 0), (0, 0, (platform[2] - (repeat * 50)), 50))
+                    window.blit(croptop, (start, platform[1]))
+                    #top and bottom hitbox
+                    if x + xvel > platform[0] - 50:
+                        if x + xvel < platform[0] + platform[2]:
+                            if y <= platform[1] - 50:
+                                if y + yvel >= platform[1] - 50:
+                                    ground = True
+                                    y = platform[1] - 50
+                                    yvel = 0
+                                else:
+                                    ground = False
+                
+                            if y >= platform[1] + platform[3]:
+                                if y + yvel < platform[1] + platform[3]:
+                                    y  = platform[1] + platform[3]
+                                    yvel = 0
+
+                        else:
+                            ground = False
+
+                    #side hitboxes
+
+                    if y > platform[1] - 50:
+                        if y < platform[1] + platform[3]:
+                            if x + 50 <= platform[0]:
+                                if x + 50 + xvel > platform[0]:
+                                    xvel = 0
+                                    x = platform[0] - 50        
+                            if x >= platform[0] + platform[2]:
+                                if x + xvel < platform[0] + platform[2]:
+                                    xvel = 0
+                                    x = platform[0] + platform[2]
+
+            if x + 50 + xvel > windowx:
+                if level < level_num:
+                    x = windowx - 50
+                    xvel = 0
+                    pygame.time.delay(90)
+                    level += 1
+                    x = startx
+                    y = starty
+                elif level >= level_num:
+                    x = windowx - 50
+                    xvel = 0
+
+            if x + xvel < 0:
+                x = 0
+                xvel = 0
+
+            if y > windowy:
+                x = startx
+                y = starty
+                    
+    
+            #Gravity, momemtum, friction, and movement
+
+            if xvel < 0:
+                xvel += 1
+            if xvel > 0:
+                xvel -= 1
+    
+            if ground == False:
+                if yvel < 20:
+                    yvel += 1
+            y += yvel
+            x += xvel
+
+        
+            pygame.display.update() #Update display
             
-                if y >= platform[1] + platform[3]:
-                    if y + yvel < platform[1] + platform[3]:
-                        y  = platform[1] + platform[3]
-                        yvel = 0
+            pygame.time.delay(30) #Tick delay
 
-        #side hitboxes
-
-        if y > platform[1] - 50:
-            if y < platform[1] + platform[3]:
-                if x + 50 <= platform[0]:
-                    if x + 50 + xvel > platform[0]:
-                        xvel = 0
-                        x = platform[0] - 50        
-                if x >= platform[0] + platform[2]:
-                    if x + xvel < platform[0] + platform[2]:
-                        xvel = 0
-                        x = platform[0] + platform[2]
-    
-    #Gravity, momemtum, friction, and movement
-
-    if xvel < 0:
-        xvel += 1
-    if xvel > 0:
-        xvel -= 1
-
-    if ground == False:
-        yvel += 1
-    y += yvel
-    x += xvel
-    
-    pygame.display.update() #Update display
-            
-    pygame.time.delay(30) #Tick delay
-
-pygame.quit() #Closes window when loop is borken by hitting the exit button
+        pygame.quit() #Closes window when loop is borken by hitting the exit button
